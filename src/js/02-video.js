6f33c17bae20cf7fb09b8c5fecbe0960f697272c
tassarian <122233433+tassarian@users.noreply.github.com>
2023-04-21 20:32:47 +0300

    js-hs-01

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,19 @@
-// import Player from "@vimeo/player"
+import Player from "@vimeo/player"
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+    
+}
+player.on('timeupdate', throttle(onPlay, 1000))
 
 
-// const iframe = document.querySelector('iframe');
-
-// const player = new Player(iframe);
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
+player.setCurrentTime(
+    JSON.parse(
+        localStorage.getItem("videoplayer-current-time")
+    ).seconds
+)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+form.addEventListener('input', throttle(saveInput, 500));
+
+if (localStorage.getItem("feedback-form-state")) {
+    emailInput.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
+    messageInput.value = JSON.parse(localStorage.getItem("feedback-form-state")).message
+}
+
+feedbackForm = {}
+
+function saveInput() {
+    feedbackForm.email = emailInput.value;
+    feedbackForm.message = messageInput.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackForm))
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.clear();
+    form.reset();
+})
