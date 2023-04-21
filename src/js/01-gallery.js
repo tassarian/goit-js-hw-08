bf43dd990b651339737f693e2b3f8bdcdb99d63d
tassarian <122233433+tassarian@users.noreply.github.com>
2023-04-21 12:33:20 +0300

    initial commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from "simplelightbox"
+
+import "simplelightbox/dist/simple-lightbox.min.css"
+
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+gallery.style.listStyle = 'none'
+
+const imgCollection = galleryItems
+    .map(({ original, description, preview }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>
+ `
+    )
+    .join('');
+
+gallery.insertAdjacentHTML('beforeend', imgCollection)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsPosition: 'bottom',
+    captionsDelay: 250,
+    captionsData: 'alt',
+    scrollZoom: false
+    })
